f038b70c009a81de550635e21d925b53fc68ef6e
Alejandro Burbano <Julianbur16@gmail.com>
2023-08-16 01:46:45 -0500

    second commit

--- database/migrations/2023_08_16_064324_create_categories_table.php
+++ database/migrations/2023_08_16_064324_create_categories_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('boxes', function (Blueprint $table) {
-
-            $table->string('Categoria')->nullable();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->string('Categoria');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('boxes');
+        Schema::dropIfExists('categories');
     }
 };
