1cc91a74f937b46f0d7cf210d3c8718dc642aba3
Alejandro Burbano <Julianbur16@gmail.com>
2023-08-16 01:33:49 -0500

    second commit

--- database/migrations/2023_05_27_222153_create_boxes_table.php
+++ database/migrations/2023_05_27_222153_create_boxes_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('boxes', function (Blueprint $table) {
-        
-            $table->string('Name');
+
+            $table->string('Categoria')->nullable();
             $table->timestamps();
         });
     }
